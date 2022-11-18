d7811975b3aa5ebc3f24e27f41baf26850e6a302
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-18 14:52:54 +0900

    seed airports table with 50 IATA codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'busiest_airports.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
+csv.each do |row|
+  a = Airport.new
+  a.iata_code = row['IATA Code']
+  a.save
+  puts "Airport with IATA code #{a.iata_code} saved"
+end
+
+puts "There are now #{Airport.count} rows in the airports table"
