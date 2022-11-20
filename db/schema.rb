bd6bd5540b888d9929c133f60e7426a8c790d89b
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-20 20:33:02 +0900

    add unique index to IATA codes and reset db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_081128) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_113023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_081128) do
     t.string "iata_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["iata_code"], name: "index_airports_on_iata_code", unique: true
   end
 
   create_table "flights", force: :cascade do |t|
