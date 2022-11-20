62bad51ba501ad9e572024ed43098e56db18b764
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-20 19:42:17 +0900

    add Flight model validation to prevent flight to same airport

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  validates :arrival_airport_id, comparison: { other_than: :departure_airport_id}
 end
