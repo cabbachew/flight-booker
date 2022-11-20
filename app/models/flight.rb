a434f10659fe9e76f5a29ade25283397f2677ad9
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-20 21:03:05 +0900

    validate presence of datetime and duration in Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,5 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
+  # validates_comparison_of
   validates :arrival_airport_id, comparison: { other_than: :departure_airport_id}
+  validates :datetime, :duration, presence: true
 end
