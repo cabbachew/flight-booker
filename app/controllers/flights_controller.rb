1dd72f8c6d4f6e8ebf2d02cfbde333b1e0eb396d
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-21 15:34:42 +0900

    filter flights by departure and arrival airports

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,12 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.pluck(:iata_code, :id).unshift(["--", nil])
     @flights = Flight.all
+    if params[:departure_airport_id].present?
+      @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
+    end
+    if params[:arrival_airport_id].present?
+      @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id])
+    end
   end
 end
