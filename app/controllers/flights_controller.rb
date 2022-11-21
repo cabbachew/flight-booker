131b511f99cd6c3f7c03b16cb76bda414d977707
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-21 15:50:01 +0900

    refactor filter_object method and make private

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,16 +1,19 @@
 class FlightsController < ApplicationController
+  FILTER_PARAMS = [:departure_airport_id, :arrival_airport_id]
+
   def index
     @airport_options = Airport.all.pluck(:iata_code, :id).unshift(["--", nil])
     @flights = Flight.where(filter_object)
   end
   
+  private 
+  
   def filter_object
     obj = {}
-    if params[:departure_airport_id].present?
-      obj[:departure_airport_id] = params[:departure_airport_id]
-    end
-    if params[:arrival_airport_id].present?
-      obj[:arrival_airport_id] = params[:arrival_airport_id]
+    FILTER_PARAMS.each do |key|
+      if params[key].present?
+        obj[key] = params[key]
+      end
     end
     obj
   end
