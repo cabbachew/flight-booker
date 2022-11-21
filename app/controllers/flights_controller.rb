7506e12ece012d655adc1a0f4246da7037928bb2
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-21 15:44:58 +0900

    define filter_object method to refactor flight filtering

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,17 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.pluck(:iata_code, :id).unshift(["--", nil])
-    @flights = Flight.all
+    @flights = Flight.where(filter_object)
+  end
+  
+  def filter_object
+    obj = {}
     if params[:departure_airport_id].present?
-      @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
+      obj[:departure_airport_id] = params[:departure_airport_id]
     end
     if params[:arrival_airport_id].present?
-      @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id])
+      obj[:arrival_airport_id] = params[:arrival_airport_id]
     end
+    obj
   end
 end
