f30c7adf1c947c513c2bef5794117da945a87dcb
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-22 14:38:51 +0900

    implement search by date and define selected query values

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,39 +1,57 @@
 class FlightsController < ApplicationController
   # FILTER_PARAMS = [:departure_airport_id, :arrival_airport_id]
   # Map params submitted under different name to correct filter params:
-  MAPPED_PARAMS = {
-    departure_code: :departure_airport_id,
-    arrival_code: :arrival_airport_id
-  }
+  # MAPPED_PARAMS = {
+  #   departure_code: :departure_airport_id,
+  #   arrival_code: :arrival_airport_id,
+  #   date: :date
+  # }
 
   def index
+    @airports = Airport.all.order(:iata_code)
     # Not needed using collection_select helper with :include_blank option
     # @airport_options = Airport.all.pluck(:iata_code, :id).unshift(["--", nil])
-    @flights = Flight.where(filter_params)
+    
+    # @flights = Flight.where(filter_params)
+    @flights = Flight.all
+    @query_params = query_params
+    if query_params[:departure_code].present?
+      @flights = @flights.where(departure_airport_id: query_params[:departure_code])
+    end
+    if query_params[:arrival_code].present?
+      @flights = @flights.where(arrival_airport_id: query_params[:arrival_code])
+    end
+    if query_params[:date].present?
+      @flights = @flights.where(date: query_params[:date])
+    end
   end
   
   private 
   
-  def filter_params
-    obj = {}
-    # FILTER_PARAMS.each do |key|
-    #   if params[key].present?
-    #     obj[key] = params[key]
-    #   end
-    # end
-    MAPPED_PARAMS.each do |key, value|
-      if query_params[key].present?
-        obj[value] = query_params[key]
-      end
-    end
-    obj
-  end
+  # def filter_params
+  #   obj = {}
+  #   # FILTER_PARAMS.each do |key|
+  #   #   if params[key].present?
+  #   #     obj[key] = params[key]
+  #   #   end
+  #   # end
+  #   MAPPED_PARAMS.each do |key, value|
+  #     if query_params[key].present?
+  #       obj[value] = query_params[key]
+  #     end
+  #   end
+  #   obj
+  # end
 
   def query_params # Strong parameters
     params.fetch(:query, {}).permit(:departure_code, :arrival_code, :num_tickets, :date)
   end
 
-  def query_date
-    
-  end
+  # def query_date
+  #   Date.new(
+  #     query_params["date(1i)"].to_i,
+  #     query_params["date(2i)"].to_i, 
+  #     query_params["date(3i)"].to_i
+  #   )
+  # end
 end
