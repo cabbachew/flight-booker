7d52e513e7b63bac9caa34c59dae8652556815f7
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-22 11:56:50 +0900

    map params submitted under different names to filter params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,19 +1,29 @@
 class FlightsController < ApplicationController
-  FILTER_PARAMS = [:departure_airport_id, :arrival_airport_id]
+  # FILTER_PARAMS = [:departure_airport_id, :arrival_airport_id]
+  # Map params submitted under different name to correct filter params:
+  MAPPED_PARAMS = {
+    departure_code: :departure_airport_id,
+    arrival_code: :arrival_airport_id
+  }
 
   def index
     # Not needed using collection_select helper with :include_blank option
     # @airport_options = Airport.all.pluck(:iata_code, :id).unshift(["--", nil])
-    @flights = Flight.where(filter_object)
+    @flights = Flight.where(filter_params)
   end
   
   private 
   
-  def filter_object
+  def filter_params
     obj = {}
-    FILTER_PARAMS.each do |key|
+    # FILTER_PARAMS.each do |key|
+    #   if params[key].present?
+    #     obj[key] = params[key]
+    #   end
+    # end
+    MAPPED_PARAMS.each do |key, value|
       if params[key].present?
-        obj[key] = params[key]
+        obj[value] = params[key]
       end
     end
     obj
