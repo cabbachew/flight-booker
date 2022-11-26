86b14a162217bd803ad7c065b76e199724270bcf
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-26 13:36:30 +0900

    add model validations for Passenger first/last name and email

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :bookings
   has_many :flights, through: :bookings
+
+  validates :first_name, :last_name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
