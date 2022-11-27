6d72fa7bbec50e40f55f1aaf8d16dd33c66504be
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-27 19:15:45 +0900

    implement bookings#create using strong parameters

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,23 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    params[:num_tickets].to_i.times { @booking.passengers.build }
   end
   
   def create
-    @booking = Booking.new(flight_id: params[:flight_id], passenger_id: params[:passenger_id])
-    flash.now[:notice] = "hi"
-    render :new, status: :unprocessable_entity
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      flash[:notice] = "Booking created succesfully!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Booking cannot be created."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email])
   end
 end
