a7667fc411632874f139bab0984cdbd90d1125ea
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-27 13:14:55 +0900

    add presence validations for Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :flight
   belongs_to :passenger
+
+  validates :flight_id, :passenger_id, presence: true # Simple Form will mark fields as required
 end
