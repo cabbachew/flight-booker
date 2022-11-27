befcf9af62553936cc99cdfc0468a8d1eff54742
cabbachew <102335309+cabbachew@users.noreply.github.com>
2022-11-27 15:03:58 +0900

    scope flight selection params and set up simple form for new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,8 +2,10 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
-
+  
   def create
-
+    @booking = Booking.new(flight_id: params[:flight_id], passenger_id: params[:passenger_id])
+    flash.now[:notice] = "hi"
+    render :new, status: :unprocessable_entity
   end
 end
